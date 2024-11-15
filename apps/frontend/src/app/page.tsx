d6b613600182423886105872608e5468c20bb632
moons-14 <moons@moons14.com>
2024-11-16 00:25:59 +0900

    layout

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,3 +1,20 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 export default function Home() {
-	return <></>;
+	return (
+		<>
+			<div className="flex">
+				<div className="flex-1 bg-red-200 h-[calc(100dvh-3rem)] border-r border-border">
+					Paint and GUI Editor
+				</div>
+				<div className="flex-1">
+					<ScrollArea className="h-[calc(100dvh-3rem)] w-full">
+						<div className="h-40 border-b border-border bg-green-200">LaTeX Preview Render</div>
+						<div className="h-40 border-b border-border bg-blue-200">LaTex Code Editor</div>
+						<div className="h-40 bg-orange-200">Chat UI with AI</div>
+					</ScrollArea>
+				</div>
+			</div>
+		</>
+	);
 }
